3c9ae43b5e80e8d3e62794c1639138f4426fe759
ravelcravo <ravelcravo@gmail.com>
2023-01-19 22:31:38 -0300

    Adicionar arquivos de projeto.

---
+++ TirarPrintForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TirarPrintForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btPrintForm = new System.Windows.Forms.Button();
+            this.btPDF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btPrintForm
+            // 
+            this.btPrintForm.Location = new System.Drawing.Point(212, 356);
+            this.btPrintForm.Name = "btPrintForm";
+            this.btPrintForm.Size = new System.Drawing.Size(151, 49);
+            this.btPrintForm.TabIndex = 0;
+            this.btPrintForm.Text = "Print Form";
+            this.btPrintForm.UseVisualStyleBackColor = true;
+            this.btPrintForm.Click += new System.EventHandler(this.btPrintForm_Click);
+            // 
+            // btPDF
+            // 
+            this.btPDF.Location = new System.Drawing.Point(402, 356);
+            this.btPDF.Name = "btPDF";
+            this.btPDF.Size = new System.Drawing.Size(151, 49);
+            this.btPDF.TabIndex = 1;
+            this.btPDF.Text = "PDFprint";
+            this.btPDF.UseVisualStyleBackColor = true;
+            this.btPDF.Click += new System.EventHandler(this.btPDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btPDF);
+            this.Controls.Add(this.btPrintForm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btPrintForm;
+        private System.Windows.Forms.Button btPDF;
+    }
+}
+
---
+++ TirarPrintForm/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace TirarPrintForm
+{
+    public partial class Form1 : Form
+    {
+        //string nomeArquivo = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btPrintForm_Click(object sender, EventArgs e)
+        {
+            //pasta onde vai salvar
+            var path = @"C:\\Users\\POSITIVO\\Desktop\\C#\\PrintForm";
+
+            var result = CapturaForm(path,this);
+
+            if (result != "")
+            {
+                MessageBox.Show("Formulario capturado em " + result);
+            }
+            else
+            {
+                MessageBox.Show("Não foi possivel capturar o form");
+            }
+         
+        }
+
+        private string CapturaForm(string path, Form form)
+        {
+            try
+            {
+                //definindo nome do arquivo
+                var nomeArquivo = path + "\\" + DateTime.Now.ToString().Replace(" ", "_").Replace("/", "").Replace(":", "") + ".png";
+                //print
+                using (var bmp = new Bitmap(form.Width,form.Height))
+                {
+                    form.DrawToBitmap(bmp, new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height));
+                    bmp.Save(nomeArquivo);
+                }
+                return nomeArquivo;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Falha: " + ex.Message);
+                return "";
+            }
+        }
+
+        private void btPDF_Click(object sender, EventArgs e)
+        {
+            string dataeHoraAtual = DateTime.Now.ToString().Replace(" ", "_").Replace("/", "").Replace(":", "");
+
+            //caminho onde vai criar o arquivo pdf
+            var caminho = @"C:\\Users\\POSITIVO\\Desktop\\C#\\PrintForm\\";
+            //nome que este pdf terá
+            string nomeArquivo = caminho + dataeHoraAtual + "turmas.pdf";
+            //criando o arquivo
+            FileStream arquivoPDF = new FileStream(nomeArquivo, FileMode.Create);
+            //definindo tamanho da pagina
+            Document doc = new Document(PageSize.A4);
+            PdfWriter escritorPDF = PdfWriter.GetInstance(doc, arquivoPDF);
+
+            iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(caminho + "19012023_221935.png");
+            logo.ScaleToFit(700f, 300f);
+            logo.Alignment = Element.ALIGN_CENTER;
+
+            doc.Open();
+            doc.Add(logo);
+            doc.Close();
+        }
+
+       
+    }
+
+
+}
